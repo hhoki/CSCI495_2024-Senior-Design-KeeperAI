6847701e04bd185f8f59dfc14fe29a3ff2ca3d6f
Ethan Buchanan <ethcbuch@ut.utm.edu>
2024-10-19 15:41:27 -0500

    Oops.

--- KeeperAI/src/server.js
+++ KeeperAI/src/server.js
@@ -10,7 +10,7 @@ app.use(cors({
   origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
-})
+}))
 
 app.use(express.json());
 
